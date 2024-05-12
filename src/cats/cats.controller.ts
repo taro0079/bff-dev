ae2ecd15d2a322778e5a07dc1f8a76901269002e
Taro Morita <morita0079@gmail.com>
2024-05-12 21:45:33 +0900

    chore: Update CatsController to fetch additional data

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -10,6 +10,32 @@ type Product = {
     productName: string;
 }
 
+type DeliveryStop = {
+    id: number;
+    prefectureCode: string;
+    notes: string;
+}
+
+type Admin = {
+    id: number;
+    name: string;
+    compony: string;
+    email: string;
+}
+
+type Calendar = {
+    id: number;
+    typeCalendar: string;
+    calendarPattern: string;
+    calendarDateYmd: string;
+    typeWorkHoliday: string;
+    typePublicHoliday: string;
+    createdBy: number;
+    updatedBy: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
 @Controller('cats')
 export class CatsController {
     constructor(private readonly catsService: CatsService) {}
@@ -19,21 +45,62 @@ export class CatsController {
 
     @Get()
     async findAll(): Promise<string> {
-        const products = await this.catsService.fetchProducts();
+
+        const [products, customers, DeliveryStop, admins, calendars] = await Promise.all([
+            this.catsService.fetchProducts(),
+            this.catsService.fetchCustomers(),
+            this.catsService.fetchDeliveryStop(),
+            this.catsService.fetchAdmins(),
+            this.catsService.fetchCalendars()
+        ]);
+        // const products = await this.catsService.fetchProducts();
         const formedProducts = products['data'].map((product) => {
             return {
                 productCode: product.productCode,
                 productName: product.productName,
             } as Product;
         });
-        const customers = await this.catsService.fetchCustomers();
+        // const customers = await this.catsService.fetchCustomers();
         const formedCustomers = customers['data'].map((customer) => {
             return {
                 id: customer.id,
                 name: customer.fullName,
             } as Customer;
         });
-        return JSON.stringify({ products: formedProducts, customers: formedCustomers });
+
+        const formedDeliveryStop = DeliveryStop['data'].map((deliveryStop) => {
+            return {
+                id: deliveryStop.id,
+                prefectureCode: deliveryStop.prefectureCode,
+                notes: deliveryStop.notes,
+            } as DeliveryStop;
+        });
+
+        const formedAdmin = admins['data'].map((admin) => {
+            return {
+                id: admin.id,
+                name: admin.name,
+                compony: admin.compony,
+                email: admin.email,
+            } as Admin;
+        });
+
+        const formedCalendars = calendars['data'].map((calendar) => {
+            return {
+                id: calendar.id,
+                typeCalendar: calendar.typeCalendar,
+                calendarPattern: calendar.calendarPattern,
+                calendarDateYmd: calendar.calendarDateYmd,
+                typeWorkHoliday: calendar.typeWorkHoliday,
+                typePublicHoliday: calendar.typePublicHoliday,
+                createdBy: calendar.createdBy,
+                updatedBy: calendar.updatedBy,
+                createdAt: calendar.createdAt,
+                updatedAt: calendar.updatedAt,
+            } as Calendar;
+        });
+
+        return JSON.stringify({ products: formedProducts, customers: formedCustomers, deliveryStop: formedDeliveryStop, admin: formedAdmin, calendars: formedCalendars});
 
     }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,13 +3,31 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class CatsService {
     async fetchProducts() {
-        const response = await fetch('http://localhost:8000/api/products');
+        const response = await fetch('http://localhost/api/products');
         const data = await response.json()
         return data;
     }
 
     async fetchCustomers() {
-        const response = await fetch('http://localhost:8000/api/customers');
+        const response = await fetch('http://localhost/api/customers');
+        const data = await response.json()
+        return data;
+    }
+
+    async fetchDeliveryStop() {
+        const response = await fetch('http://localhost/api/shipping/delivery-stop');
+        const data = await response.json()
+        return data;
+    }
+
+    async fetchAdmins() {
+        const response = await fetch('http://localhost/api/admin');
+        const data = await response.json()
+        return data;
+    }
+
+    async fetchCalendars() {
+        const response = await fetch('http://localhost/api/shipping/calendar');
         const data = await response.json()
         return data;
     }
